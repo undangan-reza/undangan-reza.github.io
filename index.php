c7b5a1c6b0d3a5939d60efb0cf30422ad8b6952d
undangan-reza <yokofajar29@gmail.com>
2025-03-24 17:51:40 +0700

    update

--- index.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-  <script>
-    <?php include('Components/header.php') ?>
-    <?php include('Components/body.php') ?>
-    <?php include('Components/script.php') ?>
-  </script>
-</html>
